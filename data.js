a32c9bba7cb2b72ba00d3d6e1ed45217a08a454b
Palakpruthi25 <dineshpruthi12345@gmail.com>
2024-07-01 16:01:29 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'C:\Users\palak\Downloads\Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'C:\Users\palak\Downloads\cover 1 (1).png'
+    },
+    {
+        name: 'song 2',
+        path: 'C:\Users\palak\Downloads\Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'C:\Users\palak\Downloads\cover 2.png'
+    },
+  {
+    name: 'song 3',
+        path: 'C:\Users\palak\Downloads\Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'C:\Users\palak\Downloads\cover 3.png'
+  },
+  {
+    name: 'song 4',
+        path: 'C:\Users\palak\Downloads\Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'C:\Users\palak\Downloads\cover 4.png'
+  },
+  {
+    name: 'song 5',
+        path: 'C:\Users\palak\Downloads\Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'C:\Users\palak\Downloads\cover 5.png'
+  },
+  {
+    name: 'song 6',
+        path: 'C:\Users\palak\Downloads\Song 6.mp3',
+        artist: 'artist 6',
+        cover: 'C:\Users\palak\Downloads\cover 6.png'
+  },
+  {
+    name: 'song 7',
+        path: 'C:\Users\palak\Downloads\Song 7.mp3',
+               artist: 'artist 7',
+        cover: 'C:\Users\palak\Downloads\cover 7.png'
+  },
+  {
+    name: 'song 8',
+        path: 'C:\Users\palak\Downloads\Song 8 (1).mp3',
+        artist: 'artist 8',
+        cover: 'C:\Users\palak\Downloads\cover 8.png'
+  },
+]
